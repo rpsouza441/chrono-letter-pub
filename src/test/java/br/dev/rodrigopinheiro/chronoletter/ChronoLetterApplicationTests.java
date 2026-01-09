17af40c2d17906a1380ca50eb4b9376176eb8a2e
rpsouza441 <rpsouza@hotmail.co.uk>
2026-01-08 21:05:34 -0300

    fix: import TestcontainersConfiguration in tests

--- src/test/java/br/dev/rodrigopinheiro/chronoletter/ChronoLetterApplicationTests.java
+++ src/test/java/br/dev/rodrigopinheiro/chronoletter/ChronoLetterApplicationTests.java
@@ -2,10 +2,12 @@ package br.dev.rodrigopinheiro.chronoletter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 class ChronoLetterApplicationTests {
 
     @Test
